738bb1f2617a73a98deacadff2433551f89a1c6e
Ruben Castillo <rcastillo@raivtech.com>
2020-04-21 22:44:21 -0600

    Se crea conexion a la base de datos y se agregan usuarios y productos
    Se crea schema y resolver para agregar usuarios, hashear password y para crear productos y hacer consulta para traer todos los productos y tambien por ID

---
+++ db/resolvers.js
@@ -0,0 +1,105 @@
+
+const Usuario = require('../models/Usuario');
+const Producto = require('../models/Producto');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env'});
+
+
+const crearToken = (usuario, secreta, expiresIn) => {
+    console.log(usuario);
+    const { id, email, nombre, apellido } = usuario;
+
+    return jwt.sign( { id, email, nombre, apellido } , secreta, { expiresIn });
+}
+
+//Resolvers
+const resolvers = {
+    Query: {
+        obtenerUsuario: async (_, { token }) => {
+            const usuarioId = await jwt.verify(token, process.env.SECRETA)
+
+            return usuarioId
+        },
+        obtenerProductos: async () => {
+            try {
+                const productos = await Producto.find({});
+
+                return productos;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        obtenerProducto: async (_, { id }) => {
+            // Revisar si el producto existe
+            const producto = await Producto.findById(id);
+
+            if(!producto) {
+                throw new Error('Producto no encontrado');
+            }
+
+            return producto;
+        }
+    },
+    Mutation: {
+        nuevoUsuario: async (_, { input }) => {
+
+            const { email, password } = input;
+
+            // Revisar si el usuario ya esta registrado
+            const existeUsuario = await Usuario.findOne({email});
+            if (existeUsuario){
+                throw new Error('El usuario ya esta registrado');
+            }
+
+            // Hashear su password
+            const salt = await bcryptjs.genSalt(10);
+            input.password = await bcryptjs.hash(password, salt);
+
+            try {
+                // Guardarlo en la base de datos
+                const usuario = new Usuario(input);
+                usuario.save(); // Guardarlo
+                return usuario;
+            } catch(error) {
+                console.logi(error);
+            }
+        },
+        autenticarUsuario: async (_, { input }) => {
+            const { email, password } = input;
+
+            // Si el usuario existe
+            const existeUsuario = await Usuario.findOne({email});
+            if(!existeUsuario) {
+                throw new Error('El usuario no existe');
+            }
+
+            // Revisar si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+            if(!passwordCorrecto) {
+                throw new Error('El password es incorrecto');
+            }
+
+            // Crear el token
+            return {
+                token: crearToken( existeUsuario, process.env.SECRETA, '24h' )
+            }
+            
+        },
+        nuevoProducto: async (_, { input }) => {
+            try {
+                const producto = new Producto(input);
+
+                //Almacenar en la base de datos
+
+                const resultado = await producto.save();
+                
+                return resultado;
+            } catch(error) {
+                console.log(error);
+            }
+        }
+    }
+}
+
+module.exports = resolvers;
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const { ApolloServer, gql } = require('apollo-server');
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
+
+const conectarDB = require('./config/db')
+
+// Conectar a la base de datos
+conectarDB();
 
 //Servidor
-const server = new ApolloServer();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 //Arrancar el servidor
 server.listen().then( ({url}) => {
