6a5ae6497e95e67207062e283cffc5496240177e
Ruben Castillo <rcastillo@raivtech.com>
2020-08-18 21:14:58 -0600

    react

--- crmcliente/components/Header.js
+++ crmcliente/components/Header.js
@@ -18,6 +18,10 @@ const Header = () => {
     //Routing
     const router = useRouter();
 
+    if(!localStorage.getItem('token')){
+        router.push('/login');
+    }
+
     // Query de Apollo
     const { data, loading, error } = useQuery(OBTENER_USUARIO);
     // console.log(data);
